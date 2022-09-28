3f6f99d90b85b88609d64fd5b1075db86e27a34c
AWDn0n <pablo.ribas.borrego@gmail.com>
2022-09-28 07:55:38 +0100

    Upload HandsOn01

---
+++ HandsOn01_22041278.R
@@ -0,0 +1,32 @@
+# ID SCRIPT -----------------------------------------------------
+
+## LEGUAJES DE PROGRAMACION ESTADISTICA
+## PROFESOR : CHRISTIAN SUCUZHANAY AREVALO
+## ALUMNO : PABLO RIBAS BORREGO, EXP 22041278
+## HANDS ON 01
+
+
+# SHORTCUTS -----------------------------------------------------
+## crtl + l = clean console
+## crtl + shift + r = new section
+
+
+# GIT COMMANDS --------------------------------------------------
+
+
+# TERMINAL COMMANDS ---------------------------------------------
+
+
+# LOADING LIBS --------------------------------------------------
+install.packages("tidyverse","httr","janitor")
+
+## Tidyverse - Ordenación de datos
+## HTTR - Salir a Internet
+## Janitor - Limpieza de datos
+
+
+# BASIC OPERATORS -----------------------------------------------
+
+
+# GETTING DATA FROM INTERNET ------------------------------------
+
